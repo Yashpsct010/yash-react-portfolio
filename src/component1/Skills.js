85851c78d19fa57264625e9080aebe278a299b4e
Yash Parmar <0808io201056.ies@ipsacademy.org>
2023-10-14 12:16:02 +0530

    skills section

--- src/component1/Skills.js
+++ src/component1/Skills.js
@@ -9,106 +9,79 @@ export const Skills = () => {
 <div className="skills" id='skills'>
 <h3>SKILLS</h3>
     <section id="skills-roadmap">
-        <h2>Web Development :-</h2>
-
+        <h2>Web Development</h2>
+        <div className="skillss">
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faHtml5} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faCss3} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faReact} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faNodeJs} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><img src={mongodb} alt="mongodb_logo"/></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faSass} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><img src={ajax} alt="ajax_logo"/></div>
-            <div className="skill-details">
-            </div>
+        </div>
         </div>
 
     </section>
 
     <section id="skills-roadmap">
-        <h2>Programming Languages :-</h2>
-
+        <h2>Programming Languages</h2>
+        <div className="skillss">
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faPhp} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faJs} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon">C++</div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon">SQL</div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faPython} /></div>
-            <div className="skill-details">
-            </div>
+        </div>
         </div>
 
     </section>
 
     <section id="skills-roadmap">
-        <h2>Technical Skills :-</h2>
-
+        <h2>Technical Skills</h2>
+        <div className="skillss">
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faGit} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon"><FontAwesomeIcon icon={faGithub} /></div>
-            <div className="skill-details">
-            </div>
         </div>
 
         <div className="skill">
             <div className="skill-icon">Restful API</div>
-            <div className="skill-details">
-            </div>
+        </div>
         </div>
 
     </section>
